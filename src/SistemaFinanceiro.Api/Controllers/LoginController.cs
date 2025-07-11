93fde9f454f0335ee0466711ee20f5e6eaa01b20
cantesani <cantesani@hotmail.com>
2025-07-11 20:39:31 -0300

    remove using

--- src/SistemaFinanceiro.Api/Controllers/LoginController.cs
+++ src/SistemaFinanceiro.Api/Controllers/LoginController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using SistemaFinanceiro.Application.UseCases.Login;
 using SistemaFinanceiro.Communication.Requests.Login;
 using SistemaFinanceiro.Communication.Requests.Users;
